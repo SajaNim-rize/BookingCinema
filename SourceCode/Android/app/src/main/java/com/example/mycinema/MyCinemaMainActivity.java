86462236299afc1c5950c37c959edd2de3802c15
JoA Ram <kramforu123@sch.ac.kr>
2022-08-22 21:11:13 +0900

    [MODIFIED] 2022.08.22 add main layout

---
+++ SourceCode/Android/app/src/main/java/com/example/mycinema/MyCinemaMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mycinema;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyCinemaMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_cinema_main);
+    }
+}
